d76638d3d8a9ae26d4de4ca547c4fb8a3a12584c
alefra88 <efra.rlz@gmail.com>
2023-05-15 03:23:12 -0600

    rendering jsx from REACT

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
-ReactDOM.render(
-  <h1>Hello world from REACT</h1>,
-  document.getElementById("root")
+
+const sample = (
+  <div>
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+      width="40px"
+      height="40px"
+    ></img>
+    <h1>Fun Facts About React</h1>
+    <ul>
+      <li>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod qui fuga
+        totam quam laborum ipsa perspiciatis et iusto quibusdam! Sapiente minima
+        totam corrupti blanditiis debitis illum fuga rem cum molestiae.
+      </li>
+      <li>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat
+        labore minima numquam commodi illum mollitia consectetur. Rem asperiores
+        quidem modi nulla, id animi quas expedita eos veritatis dolore harum
+        numquam.
+      </li>
+      <li>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        facere dolorem excepturi cumque ex! Perspiciatis ad aut voluptatibus
+        temporibus, nulla aspernatur deserunt veritatis a suscipit quisquam!
+        Maiores officiis placeat dolorum.
+      </li>
+      <li>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, est, sit
+        odit labore asperiores perspiciatis cum earum voluptatibus ipsa nam
+        expedita! Ipsa, labore asperiores saepe vel facilis praesentium animi
+        ipsam!
+      </li>
+      <li>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores
+        dolorum quaerat et cupiditate quia aliquam repellat similique officia
+        fugit vel aspernatur at, illum suscipit voluptas sint quam ipsa harum
+        porro.
+      </li>
+    </ul>
+  </div>
 );
+
+ReactDOM.render(sample, document.getElementById("root"));
